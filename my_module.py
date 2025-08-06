470be94149130a736dfb7f48f32651b70bab7423
Yabaganz <93832084+Yabaganz@users.noreply.github.com>
2025-08-06 23:26:15 +0100

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,7 @@
+def reverse_string_slicing(s):
+    return s[::-1]
+
+# Example usage:
+original_string = "Hello, Python!"
+reversed_string = reverse_string_slicing(original_string)
+print(reversed_string)
